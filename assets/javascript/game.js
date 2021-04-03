3d77f82a3ae23a078e08c7d0138d5f8017aebc3d
CodingNav <navleenkaurr@gmail.com>
2021-04-02 21:19:58 -0400

    Created an animation function

---
+++ assets/javascript/game.js
@@ -0,0 +1,21 @@
+
+var animationInterval;
+
+
+function animations(folderName, animationName) {
+    var i = 0;
+
+    clearInterval(animationInterval);
+
+    animationInterval = setInterval(function () {
+        $("#pirate1").attr("src", "./assets/images/" + folderName + "/" + folderName + "_entity_000_" + animationName + "_00" + i + ".png");
+        i++;
+
+        if (i == 7) {
+            i = 0;
+        }
+    }, 150)
+}
+
+animations(2, "ATTACK")
+animations(2, "DIE")
