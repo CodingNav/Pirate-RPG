ef19124b08fedd8a9af3786e55e5dc82d06e21c3
CodingNav <navleenkaurr@gmail.com>
2021-04-06 23:54:17 -0400

    Set up the player/enemy selection

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,14 +1,56 @@
 
 var animationInterval;
+var playerSelected = false;
+
+$(".pirate").click(function () {
+    if (playerSelected == false) {
+        if (this.id == "pirateOne") {
+            playerSelected = 1;
+            $(this).hide();
+        }
+        else if (this.id == "pirateTwo") {
+            playerSelected = 2;
+            $(this).hide();
+        }
+        else {
+            playerSelected = 3;
+            $(this).hide();
+        }
+    }
+    else {
+        if (this.id == "pirateOne") {
+            $("#mainScreen").hide();
+        }
+        else if (this.id == "pirateTwo") {
+            $("#mainScreen").hide();
+        }
+        else {
+            $("#mainScreen").hide();
+        }
+        if (playerSelected == 1) {
+            animations(1, "_entity_000_", "WALK")
+        }
+        else if (playerSelected == 2) {
+            animations(2, "_entity_000_", "WALK")
+        }
+        else {
+            animations(3, "_3-PIRATE_", "WALK")
+        }
+    }
+})
 
 
-function animations(folderName, animationName) {
+
+
+
+
+function animations(folderName, animationEntity, animationName) {
     var i = 0;
 
     clearInterval(animationInterval);
 
     animationInterval = setInterval(function () {
-        $("#pirate1").attr("src", "./assets/images/" + folderName + "/" + folderName + "_entity_000_" + animationName + "_00" + i + ".png");
+        $("#pirate1").attr("src", "./assets/images/" + folderName + "/" + folderName + animationEntity + animationName + "_00" + i + ".png");
         i++;
 
         if (i == 7) {
@@ -16,6 +58,3 @@ function animations(folderName, animationName) {
         }
     }, 150)
 }
-
-// animations(2, "ATTACK")
-// animations(2, "DIE")
